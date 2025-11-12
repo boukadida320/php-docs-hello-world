100e88130fb89cc278392e10a6d2016c9075630e
boukadida320 <ayaboukadida3@gmail.com>
2025-11-12 12:19:40 +0100

    new word

--- index.php
+++ index.php
@@ -1,4 +1,24 @@
 <?php
 
 echo "Hello azure!";
+$nom = "Aya";
+
+// Affichage d'un message
+echo "Bonjour, " . $nom . " !<br>";
+
+// Opération mathématique
+$a = 10;
+$b = 5;
+$sum = $a + $b;
+
+echo "La somme de $a et $b est : $sum.<br>";
+
+// Condition simple
+if ($sum > 10) {
+    echo "La somme est supérieure à 10.";
+} else {
+    echo "La somme est inférieure ou égale à 10.";
+}
+?>
+
 
